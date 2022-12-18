4de2f9cca7ea0631979bb188e22baa3c11c80140
maulanawesome5 <maulanawesome5@gmail.com>
2022-12-18 21:15:17 +0700

    SUCCSESS Memasukkan data blog di halaman admin

---
+++ 14_Membuat_Model_pada_Apps/Tutorial_Django/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    """title dan body ini akan di simpan sebagai kolom dan di konversi menjadi tabel MySQL"""
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+
+    def __str__(self):
+        """Supaya judul posting di halaman admin akan sesuai title"""
+        return f"{self.title}"
